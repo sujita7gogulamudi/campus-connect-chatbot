f86a876e0dbd3245049b3ee13f4c87a14a6112e8
Sujita <sujithagrace0@gmail.com>
2025-06-23 22:37:05 +0530

    Fix: bind Flask app to 0.0.0.0 for Render

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
 from fuzzywuzzy import fuzz
+import os
+
 
 app = Flask(__name__)
 
@@ -33,4 +35,5 @@ def ask():
     return jsonify({'reply': response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
